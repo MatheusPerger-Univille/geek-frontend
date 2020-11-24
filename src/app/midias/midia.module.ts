4f68b18c2978352ae886331d0a1369a639e2b73a
Matteus Menezes <ilustramaeda@gmail.com>
2020-11-24 19:13:37 -0300

    atualização

--- src/app/midias/midia.module.ts
+++ src/app/midias/midia.module.ts
@@ -3,18 +3,29 @@ import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { MidiasComponent } from './midias.component';
 import { SharedModule } from '../core/components/shared/shared-module.module';
+import { LivrosComponent } from './livros/livros.component';
+import { InformacoesLivroComponent } from './livros/livro/informacoes-livro/informacoes-livro.component';
+import { LivroComponent } from './livros/livro/livro.component';
 
 const ROUTES: Routes = [
   { path: '', component: MidiasComponent, pathMatch: 'full' },
   {
     path: 'filmes',
-    loadChildren: () => import('./filmes/filmes.module').then(m => m.FilmesModule)
-  }
+    loadChildren: () => import('./filmes/filmes.module').then(m => m.FilmesModule) 
+  },
+  {
+    path: 'series',
+    loadChildren: () => import('./series/series.module').then(m => m.SeriesModule)
+  },
+  
 ];
 
 @NgModule({
   declarations: [
-    MidiasComponent
+    MidiasComponent,
+    LivrosComponent,
+    InformacoesLivroComponent,
+    LivroComponent
   ],
   imports: [
     CommonModule,
